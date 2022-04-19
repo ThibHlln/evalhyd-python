3277845adf303275b7f7c457dbd69270389fcca0
Thibault Hallouin <thibault.hallouin@inrae.fr>
2022-04-19 16:02:31 +0200

    first version working for 1D arrays only

---
+++ src/evalhyd-python.cpp
@@ -0,0 +1,20 @@
+#include <pybind11/pybind11.h>
+
+#define FORCE_IMPORT_ARRAY
+#include <xtensor-python/pyarray.hpp>
+
+#include "nse.hpp"
+
+namespace py = pybind11;
+
+// Python Module and Docstrings
+
+PYBIND11_MODULE(evalhyd, m)
+{
+    xt::import_numpy();
+
+    m.doc() = "evaluator for hydrological simulations/forecasts";
+
+    m.def("nse", nse<xt::pyarray<double>>, "Return the Nash-Sutcliffe Efficiency (NSE)",
+          py::arg("sim"), py::arg("obs"), py::arg("axis") = 0);
+}
