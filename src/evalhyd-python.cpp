8fa6c965c96053594ba461d0084e192d59288c0d
Thibault Hallouin <thibault.hallouin@inrae.fr>
2022-04-26 16:28:07 +0200

    update evalhyd dependency version

--- src/evalhyd-python.cpp
+++ src/evalhyd-python.cpp
@@ -15,8 +15,8 @@ PYBIND11_MODULE(evalhyd, m)
 
     m.doc() = "evaluator for hydrological simulations/forecasts";
 
-    m.def("nse", eh::nse<xt::pytensor<double, 1>>, "Return the Nash-Sutcliffe Efficiency (NSE) [1D arrays]",
+    m.def("nse", evalhyd::nse<xt::pytensor<double, 1>>, "Return the Nash-Sutcliffe Efficiency (NSE) [1D arrays]",
           py::arg("sim"), py::arg("obs"), py::arg("axis") = 0);
-    m.def("nse", eh::nse<xt::pytensor<double, 2>>, "Return the Nash-Sutcliffe Efficiency (NSE) [2D arrays]",
+    m.def("nse", evalhyd::nse<xt::pytensor<double, 2>>, "Return the Nash-Sutcliffe Efficiency (NSE) [2D arrays]",
           py::arg("sim"), py::arg("obs"), py::arg("axis") = 0);
 }
