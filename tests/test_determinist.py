80cf8e5d9132cd0b473904521a9fe8b954216ff5
Thibault Hallouin <thibault.hallouin@inrae.fr>
2022-07-11 14:12:06 +0200

    add unit tests for Python bindings

---
+++ tests/test_determinist.py
@@ -0,0 +1,98 @@
+import unittest
+import numpy
+
+import evalhyd
+
+
+# load some predicted and observed streamflow
+_prd = numpy.genfromtxt("./data/q_prd.csv", delimiter=',')[:5, :]
+_obs = numpy.genfromtxt("./data/q_obs.csv", delimiter=',')[numpy.newaxis, :]
+
+
+class TestMetrics(unittest.TestCase):
+
+    expected = {
+        'RMSE':
+            [[777.03427238],
+             [776.87847854],
+             [777.80021654],
+             [778.15108180],
+             [778.61486998]],
+        'NSE':
+            [[0.71891219],
+             [0.71902490],
+             [0.71835777],
+             [0.71810361],
+             [0.71776748]],
+        'KGE':
+            [[0.74808767],
+             [0.74610620],
+             [0.74411103],
+             [0.74301085],
+             [0.74176777]],
+        'KGEPRIME':
+            [[0.81314075],
+             [0.81277485],
+             [0.81203242],
+             [0.81178671],
+             [0.81138658]]
+    }
+
+    def test_metrics_2d(self):
+        for metric in self.expected.keys():
+            with self.subTest(metric=metric):
+                numpy.testing.assert_almost_equal(
+                    evalhyd.evald(_obs, _prd, [metric])[0],
+                    self.expected[metric]
+                )
+
+    def test_metrics_1d(self):
+        for metric in self.expected.keys():
+            # TODO: fix failure for KGE and KGEPRIME with 1D pytensors
+            if metric not in ('KGE', 'KGEPRIME'):
+                with self.subTest(metric=metric):
+                    numpy.testing.assert_almost_equal(
+                        evalhyd.evald(_obs[0], _prd[0], [metric])[0],
+                        self.expected[metric][0]
+                    )
+
+
+class TestTransform(unittest.TestCase):
+
+    def test_transform_sqrt(self):
+        numpy.testing.assert_almost_equal(
+            evalhyd.evald(_obs, _prd, ["NSE"], "sqrt")[0],
+            evalhyd.evald(_obs ** 0.5, _prd ** 0.5, ["NSE"])[0]
+        )
+
+    def test_transform_inv(self):
+        eps = 0.01 * numpy.mean(_obs)
+        numpy.testing.assert_almost_equal(
+            evalhyd.evald(_obs, _prd, ["NSE"], "inv")[0],
+            evalhyd.evald(1 / (_obs + eps), 1 / (_prd + eps), ["NSE"])[0]
+        )
+
+    def test_transform_log(self):
+        eps = 0.01 * numpy.mean(_obs)
+        numpy.testing.assert_almost_equal(
+            evalhyd.evald(_obs, _prd, ["NSE"], "log")[0],
+            evalhyd.evald(numpy.log(_obs + eps), numpy.log(_prd + eps),
+                          ["NSE"])[0]
+        )
+
+    def test_transform_pow(self):
+        numpy.testing.assert_almost_equal(
+            evalhyd.evald(_obs, _prd, ["NSE"], "pow", exponent=0.3)[0],
+            evalhyd.evald(_obs ** 0.3, _prd ** 0.3, ["NSE"])[0]
+        )
+
+
+if __name__ == '__main__':
+    test_loader = unittest.TestLoader()
+    test_suite = unittest.TestSuite()
+
+    test_suite.addTests(
+        test_loader.loadTestsFromTestCase(TestMetrics))
+
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(test_suite)
---
+++ tests/test_probabilist.py
@@ -0,0 +1,96 @@
+import unittest
+import numpy
+
+import evalhyd
+
+
+# load some predicted and observed streamflow
+_prd = (
+    numpy.genfromtxt("./data/q_prd.csv", delimiter=',')
+    [:5, :][numpy.newaxis, numpy.newaxis, ...]
+)
+_obs = numpy.genfromtxt("./data/q_obs.csv", delimiter=',')[numpy.newaxis, :]
+
+
+class TestMetrics(unittest.TestCase):
+
+    expected_thr = {
+        'BS':
+            [[[[0.1081672, 0.073954980, 0.08681672]]]],
+        'BSS':
+            [[[[0.56240422, 0.66612211, 0.56288391]]]],
+        'BS_CRD':
+            [[[[[0.01335634, 0.15237434, 0.24718520],
+                [0.00550861, 0.15305671, 0.22150309],
+                [0.00753750, 0.11933328, 0.19861250]]]]],
+        'BS_LBD':
+            [[[[[0.01244569, 0.14933386, 0.24505537],
+                [0.00801337, 0.14745568, 0.21339730],
+                [0.01719462, 0.10479711, 0.17441921]]]]]
+    }
+
+    expected_qtl = {
+        'QS':
+            [[[[321.1607717,  294.3494105,  265.70418006,
+                236.15648446, 206.03965702]]]],
+        'CRPS':
+            [[[207.8059391]]]
+    }
+
+    def test_threshold_metrics(self):
+        thr = [690, 534, 445]
+        for metric in self.expected_thr.keys():
+            with self.subTest(metric=metric):
+                numpy.testing.assert_almost_equal(
+                    evalhyd.evalp(_obs, _prd, [metric], thr)[0],
+                    self.expected_thr[metric]
+                )
+
+    def test_quantile_metrics(self):
+        for metric in self.expected_qtl.keys():
+            with self.subTest(metric=metric):
+                numpy.testing.assert_almost_equal(
+                    evalhyd.evalp(_obs, _prd, [metric])[0],
+                    self.expected_qtl[metric]
+                )
+
+
+class TestDecomposition(unittest.TestCase):
+
+    def test_brier_calibration_refinement(self):
+        thr = [690, 534, 445]
+        bs, = evalhyd.evalp(_obs, _prd, ["BS"], thr)
+        bs_crd, = evalhyd.evalp(_obs, _prd, ["BS_CRD"], thr)
+        numpy.testing.assert_almost_equal(
+            bs, bs_crd[..., 0] - bs_crd[..., 1] + bs_crd[..., 2]
+        )
+
+    def test_brier_likelihood_base_rate(self):
+        thr = [690, 534, 445]
+        bs, = evalhyd.evalp(_obs, _prd, ["BS"], thr)
+        bs_lbd, = evalhyd.evalp(_obs, _prd, ["BS_LBD"], thr)
+        numpy.testing.assert_almost_equal(
+            bs, bs_lbd[..., 0] - bs_lbd[..., 1] + bs_lbd[..., 2]
+        )
+
+
+class TestMasking(unittest.TestCase):
+
+    def test_masks(self):
+        msk = numpy.ones(_obs.shape, dtype=bool)
+        msk[:, :99] = False
+        numpy.testing.assert_almost_equal(
+            evalhyd.evalp(_obs, _prd, ["QS"], t_msk=msk)[0],
+            evalhyd.evalp(_obs[..., 99:], _prd[..., 99:], ["QS"])[0]
+        )
+
+
+if __name__ == '__main__':
+    test_loader = unittest.TestLoader()
+    test_suite = unittest.TestSuite()
+
+    test_suite.addTests(
+        test_loader.loadTestsFromTestCase(TestMetrics))
+
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(test_suite)
