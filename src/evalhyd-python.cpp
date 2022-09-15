76c5840bc5de708d41c01af6f128af5d0347b2a7
Thibault Hallouin <thibault.hallouin@inrae.fr>
2022-09-15 14:48:49 +0200

    update docstrings for new features with masking conditions

--- src/evalhyd-python.cpp
+++ src/evalhyd-python.cpp
@@ -100,11 +100,12 @@ PYBIND11_MODULE(evalhyd, m)
                 m_cdt: `numpy.ndarray`, optional
                    1D array of masking condition(s) to use to generate
                    temporal subsets. Each condition consists in a string and
-                   can be specified on observed streamflow values or on time
-                   indices. If provided in combination with *t_msk*, the latter
-                   takes precedence. If not provided and neither is *t_msk*, no
-                   subset is performed. If provided, only one condition per
-                   time series of observations can be provided.
+                   can be specified on observed streamflow values/statistics
+                   (mean, median, quantile), or on time indices. If provided
+                   in combination with *t_msk*, the latter takes precedence.
+                   If not provided and neither is *t_msk*, no subset is
+                   performed. If provided, only one condition per time series
+                   of observations can be provided.
                    shape: (1,)
 
             :Returns:
@@ -197,11 +198,12 @@ PYBIND11_MODULE(evalhyd, m)
                 m_cdt: `numpy.ndarray`, optional
                    2D array of masking condition(s) to use to generate
                    temporal subsets. Each condition consists in a string and
-                   can be specified on observed streamflow values or on time
-                   indices. If provided in combination with *t_msk*, the latter
-                   takes precedence. If not provided and neither is *t_msk*, no
-                   subset is performed. If provided, only one condition per
-                   time series of observations can be provided.
+                   can be specified on observed streamflow values/statistics
+                   (mean, median, quantile), or on time indices. If provided
+                   in combination with *t_msk*, the latter takes precedence.
+                   If not provided and neither is *t_msk*, no subset is
+                   performed. If provided, only one condition per time series
+                   of observations can be provided.
                    shape: (1+, 1)
 
             :Returns:
@@ -264,12 +266,13 @@ PYBIND11_MODULE(evalhyd, m)
                 m_cdt: `numpy.ndarray`, optional
                     2D array of conditions to generate temporal subsets. Each
                     condition consists in a string and can be specified on
-                    observed streamflow values or on time indices. If provided
-                    in combination with t_msk, the latter takes precedence. If
-                    not provided and neither is t_msk, no subset is performed
-                    and only one set of metrics is returned corresponding to
-                    the whole time series. If provided, as many sets of metrics
-                    are returned as they are conditions provided.
+                    observed/predicted streamflow values/statistics (mean,
+                    median, quantile), or on time indices. If provided in
+                    combination with t_msk, the latter takes precedence. If not
+                    provided and neither is t_msk, no subset is performed and
+                    only one set of metrics is returned corresponding to the
+                    whole time series. If provided, as many sets of metrics are
+                    returned as they are conditions provided.
                     shape: (sites, subsets)
 
             :Returns:
