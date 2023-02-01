69a2f5f232da947163628ad9b09dfca7d6db555c
Thibault Hallouin <thibault.hallouin@inrae.fr>
2023-02-01 10:18:29 +0100

    replace "git clone" by git submodules for dependencies management
    because the workaround for the read only mode on the Windows file system
    is unstable/inconsistent

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import sys
 import os
-import subprocess
-import stat
-import shutil
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
@@ -13,52 +10,15 @@ import numpy
 with open("evalhyd/version.py", 'r') as fv:
     exec(fv.read())
 
-
 # vendor dependencies (unless told otherwise via environment variable)
-def git_clone(*args):
-    r = subprocess.run(
-        ['git', 'clone', *args, '-c', 'advice.detachedHead=false'],
-        capture_output=True
-    )
-    if r.returncode != 0:
-        raise RuntimeError(r.stderr.decode('utf-8'))
-
-
-def remove_readonly(func, path, exc):
-    # fix for Windows: change access permission for read-only files
-    # so that they can be removed
-    if not os.access(path, os.W_OK):
-        os.chmod(path, stat.S_IWUSR)
-        func(path)
-
-
-deps = [
-    ('xtl', '0.7.0',
-     'https://github.com/xtensor-stack/xtl'),
-    ('xtensor', '0.24.0',
-     'https://github.com/xtensor-stack/xtensor'),
-    ('xtensor-python', '0.26.1',
-     'https://github.com/xtensor-stack/xtensor-python'),
-    ('evalhyd', 'dev',
-     'https://gitlab.irstea.fr/HYCAR-Hydro/evalhyd/evalhyd')
-]
+deps = ['xtl', 'xtensor', 'xtensor-python', 'evalhyd']
+deps_blank_path = os.path.join(os.getcwd(), 'deps', '{}', 'include')
 
 deps_include_dirs = []
-for dep, version, url in deps:
+for dep in deps:
     if not os.getenv(f"EVALHYD_PYTHON_VENDOR_{dep.upper().replace('-', '_')}") == 'FALSE':
-        # remove existing dependency if it exists
-        dir_path = os.path.join(os.getcwd(), 'deps', dep)
-        if os.path.exists(dir_path) and os.path.isdir(dir_path):
-            shutil.rmtree(dir_path, onerror=remove_readonly)
-            print(f"removed existing {dep}")
-
-        # fetch dependency
-        git_clone(url, f'deps/{dep}', '--branch', version)
-        print(f"vendored {dep} {version}")
-
         # register dependency headers
-        deps_include_dirs.append(os.path.join(dir_path, 'include'))
-
+        deps_include_dirs.append(deps_blank_path.format(dep))
 
 # configure Python extension
 ext_modules = [
