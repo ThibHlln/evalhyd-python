64c37ef82284fbf8f4771eb10e9b781196b124e8
Thibault Hallouin <thibhlln@gmail.com>
2026-03-09 11:32:47 +0100

    add upload to actual PyPI

--- setup.py
+++ setup.py
@@ -65,6 +65,5 @@ setup(
     ext_modules=ext_modules,
     cmdclass={'build_ext': build_ext},
     install_requires=['numpy>=1.16'],
-    extras_require={'tests': 'numpy>=1.16'},
     zip_safe=False,
 )
